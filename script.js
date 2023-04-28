70bc05caa871926ef9b533b2510260a9ae5ffa05
Noah <noahhughesph@gmail.com>
2023-04-28 18:32:54 -0400

    creates all API calles to genderize.io, agify.io, and nationalize.io

--- script.js
+++ script.js
@@ -5,18 +5,27 @@ const nameParagraph = document.getElementById('nameValue');
 const countryParagraph = document.getElementById('nameCountry');
 const originParagraph = document.getElementById('nameOrigin');
 const nameGenderParagraph = document.getElementById('nameGenderOdds');
+
 let textValue = '';
 let countryValue = '';
 let nameValue = '';
 let nameOrigin = '';
 let nameGender = '';
 let nameGenderProbablity = '';
+
 searchBtn.addEventListener('click', () =>{
     textValue = textBox.value.toLowerCase();
     countryValue = countrySelect.value;
-    async function logNameData(){
-        const genderfyResponse = await fetch(`https://api.genderize.io?name=${textValue}&country_id=${countryValue}`)
-        const genderfyObj = await fetchResponse.json();
+    async function logGenderizeData(){
+        const genderizeResponse = await fetch(`https://api.genderize.io?name=${textValue}&country_id=${countryValue}`);
+        const genderizeObj = await genderizeResponse.json();
     }
-
+    async function logAgifyData(){
+        const agifyResponse = await fetch(`https://api.agify.io?name=${textValue}&country_id=${countryValue}`);
+        const agifyObj = await agifyResponse.json();
+    }
+    async function logNationalizeData(){
+        const nationalizeResponse = await fetch(`https://api.nationalize.io?name=${textValue}`);
+        const nationalizeObj = await nationalizeResponse.json();
+    }
 });
